d430afa6ed9780299f107445a52b5c004d138988
Tobrun <tobrun.van.nuland@gmail.com>
2018-11-08 14:11:12 +0100

    [android] - remove style loaded listener

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/MapView.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/MapView.java
@@ -167,7 +167,7 @@ public class MapView extends FrameLayout implements NativeMapView.ViewCallback {
 
     // MapboxMap
     mapboxMap = new MapboxMap(nativeMapView, transform, uiSettings, proj, registerTouchListener,
-      annotationManager, cameraChangeDispatcher, mapChangeReceiver);
+      annotationManager, cameraChangeDispatcher);
 
     // user input
     mapGestureDetector = new MapGestureDetector(context, transform, proj, uiSettings,
